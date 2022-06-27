b2208102ad8da63eebee073789b59ed4a75e448e
LINCOLN <lincolneulogiohuanca15@gmail.com>
2022-06-27 17:09:46 -0500

    corregimos el promp a prompt

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-let nombre = promt('cual es tu nombre');
-let edad = promt('cual es tu edad');
+let nombre = prompt('cual es tu nombre');
+let edad = prompt('cual es tu edad');
 
 document.getElementById('app').innerHTML = `Bienvenido ${nombre} de {edad} años`;
