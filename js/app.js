9f208fd914bb1037c65f1e30d9a5f7c9db92e9ec
LINCOLN <lincolneulogiohuanca15@gmail.com>
2022-06-27 17:10:47 -0500

    corregimos la variable edad

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 let nombre = prompt('cual es tu nombre');
 let edad = prompt('cual es tu edad');
 
-document.getElementById('app').innerHTML = `Bienvenido ${nombre} de {edad} años`;
+document.getElementById('app').innerHTML = `Bienvenido ${nombre} de ${edad} años`;
